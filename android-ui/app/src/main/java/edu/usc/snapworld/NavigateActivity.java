1a3062ce43126ad6814de9f1bc98e3c204d0df4d
shivensaiwal <saiwal.shiven@gmail.com>
2016-11-26 15:58:29 -0800

    Navigation Google Maps

--- android-ui/app/src/main/java/edu/usc/snapworld/NavigateActivity.java
+++ android-ui/app/src/main/java/edu/usc/snapworld/NavigateActivity.java
@@ -1,8 +1,11 @@
 package edu.usc.snapworld;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class NavigateActivity extends AppCompatActivity {
@@ -22,4 +25,19 @@ public class NavigateActivity extends AppCompatActivity {
     {
         imageView.setImageBitmap(img);
     }
+
+    public void navigateMap(View v)
+    {
+        String latitude_cur = "34.0310149";
+        String longitude_cur = "-118.288292";
+        String latitude_dest = "34.028566";
+        String longitude_dest = "-118.284314";
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr="+latitude_cur+","+longitude_cur+"&daddr="+latitude_dest+","+longitude_dest));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER );
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+        startActivity(intent);
+    }
+
 }
